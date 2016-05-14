7464c5a12e2d673c49b4b67701ba84dd1b8e5c19
kadir <kadirergun@yandex.com>
2016-05-14 19:25:30 +0300

    if(!isInEditMode()) update #1

--- gradienttextcolor/src/main/java/com/lightfire/gradienttextcolor/GradientTextView.java
+++ gradienttextcolor/src/main/java/com/lightfire/gradienttextcolor/GradientTextView.java
@@ -25,17 +25,20 @@ public class GradientTextView extends TextView {
 
     public GradientTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        if(!isInEditMode())
         createShader(context,attrs,0);
     }
 
     public GradientTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        if(!isInEditMode())
         createShader(context,attrs,defStyleAttr);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public GradientTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        if(!isInEditMode())
         createShader(context,attrs,defStyleAttr);
     }
 
